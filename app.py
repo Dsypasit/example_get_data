978c3872c82bdc81105302e5b65aea771f380fb0
pasit <ong.pasit@gmail.com>
2020-12-09 15:56:30 +0700

    example use route and database

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
-from route import test
+from route import test, get_anime, return_all
 
 data = [{'anime':["onepiece", "one piece"],'link':"alsdkfj","type":"action"},
         {'anime':["toriko"],'link':{'line':"alsdkfj"},"type":"action"},
@@ -15,6 +15,14 @@ def home():
 @app.route('/api')
 def api():
     return jsonify(test())
+
+@app.route('/get_all')
+def return_all():
+    return jsonify(get_all())
+
+@app.route('/get_name/<name>')
+def get_name(name):
+    return jsonify(get_anime())
 
 
 if __name__ == '__main__':
--- route.py
+++ route.py
@@ -1,2 +1,18 @@
 def test():
     return {'message':'hello world'}
+
+def get_anime(name):
+    return {'name': name}
+
+def return_all(conn):
+    """
+    Query all rows in the tasks table
+    :param conn: the Connection object
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM tasks")
+
+    rows = cur.fetchall()
+
+    return row
