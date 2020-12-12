2109cdc7f301def9f20dc10fdb6824e31a617321
pasit <ong.pasit@gmail.com>
2020-12-12 11:41:12 +0700

    get data

---
+++ route.py
@@ -0,0 +1,19 @@
+from data_base import anime_data
+
+def get_all():
+    return anime_data()
+
+def get_name(name):
+    for item in anime_data():
+        if name in item["name"]:
+            return item
+    else:
+        return None
+
+def get_genres(gen):
+    data=[]
+    for item in anime_data():
+        if gen in item["genres"]:
+            data.append(item)
+    return data
+
